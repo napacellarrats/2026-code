24179db6bfa799eb8313fbba03b0b43ce91c39b7
napahighrobotics <naparobotics6918@gmail.com>
2026-02-14 13:02:48 -0800

    adjust aiming values

--- 2026KitBotInline/src/main/java/frc/robot/LimelightAlign.java
+++ 2026KitBotInline/src/main/java/frc/robot/LimelightAlign.java
@@ -6,9 +6,9 @@ import frc.robot.subsystems.CANDriveSubsystem;
 
 public class LimelightAlign {
     public final Double[] limelightShootAlign() {
-        double KpAim = -0.5;
-        double KpDistance = -0.5;
-        double min_aim_command = 0.05;
+        double KpAim = -0.05;
+        double KpDistance = -0.1;
+        double min_aim_command = 0.1;
 
         double steering_adjust = 0.0;
         double distance_adjust = 0.0;
@@ -19,10 +19,10 @@ public class LimelightAlign {
 
         steering_adjust = 0.0;
 
-        if (tx > 1.0) {
-            steering_adjust = KpAim * -tx - min_aim_command;
-        } else if (tx < -1.0) {
-            steering_adjust = KpAim * -tx + min_aim_command;
+        if (tx > 1.0 && KpAim * tx -min_aim_command > 0) {
+            steering_adjust = -0.5;
+        } else if (tx < -1.0 && KpAim * tx +min_aim_command < 0) {
+            steering_adjust = 0.5;
         }
 
         distance_adjust = KpDistance * -ty;
